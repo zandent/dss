1fc0627010a41db9f9ef3951120aed88809007ef
Christopher Fredén <me@icetan.org>
2020-04-17 13:23:01 +0200

    Set solc version for dapp to 0.5.12

--- shell.nix
+++ shell.nix
@@ -1,9 +1,14 @@
-with import (fetchGit "https://github.com/makerdao/makerpkgs") {};
+{ dappPkgs ? (
+    import (fetchGit "https://github.com/makerdao/makerpkgs") {}
+  ).dappPkgsVersions.seth-0_8_4
+}:
 
-with dappPkgsVersions.seth-0_8_4;
+with dappPkgs;
 
-mkShell {
-  buildInputs = [
-    (dapp.override { inherit solc; })
-  ];
+let
+  dapp' = dapp.override {
+    solc = solc-versions.solc_0_5_12;
+  };
+in mkShell {
+  buildInputs = [ dapp' ];
 }
